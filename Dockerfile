5901517d7ae764c3a22652cc554734834ff37a39
Youngbok Yoon <bok95@bokBook.local>
2017-04-17 19:57:23 +0900

    [APIWAY-13] Node.js boilerplate App 완성

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM node:boron
 
+LABEL authors="Youngbok Yoon <master@bluehack.net>"
+
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
+
+RUN apk add --update nodejs bash git
 
 COPY package.json /usr/src/app/
 RUN npm install
---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+git_user_id=$1
+git_repo_id=$2
+git_branch=$3
+
+if [ "$git_user_id" = "" ]; then
+git_user_id="{{{gitUserId}}}"
+echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
+fi
+
+if [ "$git_repo_id" = "" ]; then
+git_repo_id="{{{gitRepoId}}}"
+echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
+fi
+
+if [ "$git_branch" = "" ]; then
+git_branch="master"
+echo "[INFO] No command line input provided. Set \$git_branch $git_branch"
+fi
+
+
+git clone https://github.com/${git_user_id}/${git_repo_id}.git ${git_repo_id}
+
+cd ${git_repo_id}
+
+if [ "$git_branch" != "master" ]; then
+git checkout ${git_branch}
+fi
+
+
