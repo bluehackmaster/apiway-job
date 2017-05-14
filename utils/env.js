d41a9d9fdf4df8aa5515fce3ce1e54286e31ae76
Youngbok Yoon <master@bluehack.net>
2017-05-14 11:51:01 +0900

    Ignoring env.js from Git to prevent expose privite keys

--- utils/env.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    secret: 'SeCrEtKeYfOrHaShInG',
-    secretEnv: {
-        GITHUB_TOKEN: "707cbdbf8727ecdc140177a9945b7ea4905f1006",
-        AWS_ACCESS_KEY_ID: "AKIAIW3GZ3HRYVYEPDSA",
-        AWS_SECRET_ACCESS_KEY: "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",
-        AWS_REGION: "ap-northeast-2"
-    }
-}
