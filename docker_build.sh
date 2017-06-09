ff17568ffd57b391e0b67562a65c5dbd62d8eddf
Youngbok Yoon <master@bluehack.net>
2017-06-09 17:23:57 +0900

    tc-runner를 apiway-job으로 명칭 변경

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPOSITORY="tower-tc-runner"
+SERVICE_NAME="apiway-job"
 TAG=$1
 AWS_CONTAINER_REGISTRY="539277938309.dkr.ecr.us-west-2.amazonaws.com"
 
@@ -9,8 +9,8 @@ aws ecr get-login --region us-west-2 > ecr_login.sh
 chmod +x ecr_login.sh
 ./ecr_login.sh
 
-docker build -t $REPOSITORY:$TAG .
-docker tag $REPOSITORY:$TAG $AWS_CONTAINER_REGISTRY/$REPOSITORY:$TAG
-docker push $AWS_CONTAINER_REGISTRY/$REPOSITORY:$TAG
+docker build -t $SERVICE_NAME:$TAG .
+docker tag $SERVICE_NAME:$TAG $AWS_CONTAINER_REGISTRY/$SERVICE_NAME:$TAG
+docker push $AWS_CONTAINER_REGISTRY/$SERVICE_NAME:$TAG
 
 rm -f ecr_login.sh
